b3fc172a62a39d62383d2e89f3225bfac9b2b8b2
javivuvix2007-cmd <javivuvix2007@gmail.com>
2026-03-11 12:19:38 +0100

    Documentación
    Documento la clase calculMental, incluyendo su función Operacion().

--- Algortimo78/src/es/iesmz/ed/algoritmes/calculMental.java
+++ Algortimo78/src/es/iesmz/ed/algoritmes/calculMental.java
@@ -1,14 +1,28 @@
 package es.iesmz.ed.algoritmes;
 
 import java.util.ArrayList;
+/**
+ * Clase que realiza operaciones de cálculo mental a partir de una cadena de texto.
+ * La cadena contiene números y operadores separados por espacios.
+ */
 
 public class calculMental {
+    /** Cadena que contiene la operación a realizar */
     private String n;
-
+    /**
+     * Constructor de la clase calculMental.
+     *
+     * @param n Cadena con la operación matemática (números y operadores separados por espacios)
+     */
     public calculMental(String n) {
         this.n = n;
     }
 
+    /**
+     * Realiza la operación indicada en la cadena y guarda los resultados parciales.
+     *
+     * @return ArrayList con los resultados después de cada operación realizada
+     */
     public ArrayList<Integer> Operacion() {
         ArrayList<Integer> resultados = new ArrayList<>();
         String[] partes = n.split(" ");
