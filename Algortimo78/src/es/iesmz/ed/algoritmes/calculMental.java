7fcbb56f1f60bde778328f6ebc4ee2a2c3d88686
javivuvix2007-cmd <javivuvix2007@gmail.com>
2026-03-11 12:10:16 +0100

    Update calculMental
    Aplico y realizo la funcion Operacion().

--- Algortimo78/src/es/iesmz/ed/algoritmes/calculMental.java
+++ Algortimo78/src/es/iesmz/ed/algoritmes/calculMental.java
@@ -1,9 +1,32 @@
 package es.iesmz.ed.algoritmes;
 
+import java.util.ArrayList;
+
 public class calculMental {
     private String n;
 
     public calculMental(String n) {
         this.n = n;
     }
+
+    public ArrayList<Integer> Operacion() {
+        ArrayList<Integer> resultados = new ArrayList<>();
+        String[] partes = n.split(" ");
+        int resultado = Integer.parseInt(partes[0]);
+
+        for (int i = 0; i < partes.length; i++) {
+            if(partes[i].equals(".")) {
+                break;
+            }
+        String o = partes[i];
+        int numero = Integer.parseInt((partes[i] + 1));
+
+        if(n.equals("+")) {
+            resultado += numero;
+        } else if (n.equals("-")) {
+            resultado -= numero;
+        }
+        resultados.add(resultado);
+    }   return resultados;
+}
 }
