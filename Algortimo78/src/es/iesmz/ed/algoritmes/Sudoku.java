1698cf02becdb03661ec420bc01a3eaf1d309e17
javivuvix2007-cmd <javivuvix2007@gmail.com>
2026-03-11 12:47:28 +0100

    Refactorizacion
    Refactorizo el codigo para que funcione perfectamente la funcion esCorrecte().

--- Algortimo78/src/es/iesmz/ed/algoritmes/Sudoku.java
+++ Algortimo78/src/es/iesmz/ed/algoritmes/Sudoku.java
@@ -6,17 +6,29 @@ public class Sudoku {
     public Sudoku(int[][] s) {
         S = s;
     }
-    public boolean esCorrecte() {
-        for (int i = 1; i < 9; i++) {
-            boolean[] v = new boolean[10];
-            for (int j = 0; j < 9; j++) {
-                int num = S[i][j];
 
-                if(v[num]) {
-                    return false;
-                }
-                v[num] = true;
+    public boolean esCorrecte() {
+        for (int i = 0; i < 9; i++) {
+            boolean[] fila = new boolean[10];
+            boolean[] columna = new boolean[10];
+            boolean[] bloque = new boolean[10];
+
+            for (int j = 0; j < 9; j++) {
+                int numFila = S[i][j];
+                if (numFila < 1 || numFila > 9 || fila[numFila]) return false;
+                fila[numFila] = true;
+
+                int numCol = S[j][i];
+                if (numCol < 1 || numCol > 9 || columna[numCol]) return false;
+                columna[numCol] = true;
+
+                int bi = 3 * (i / 3) + j / 3;
+                int bj = 3 * (i % 3) + j % 3;
+                int numBloque = S[bi][bj];
+                if (numBloque < 1 || numBloque > 9 || bloque[numBloque]) return false;
+                bloque[numBloque] = true;
             }
-        } return true;
+        }
+        return true;
     }
 }
